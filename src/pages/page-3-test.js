937b56442cbd4e06ef1cf80ca89f681341d2b28f
Mark Wheeler <markwheeler@marks-mini-2.lan>
2023-01-04 13:21:38 -0800

    test for adding a file and updating 2 files

--- src/pages/page-3-test.js
+++ src/pages/page-3-test.js
@@ -1,2 +1,14 @@
 import * as React from "react"
 import { Link } from "gatsby"
+
+const page3test = () => (
+  <Layout>
+    <h1>this is page 3 test</h1>
+    <p>Welcome to page 3</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+//export const Head = () => <Seo title="Page three" />
+
+export default SecondPage
